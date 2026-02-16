1661966217cb6e83070c3e3583e62e71b4bc44c4
kelvinrfr <10376375+kelvinrfr@users.noreply.github.com>
2026-02-16 17:33:29 -0500

    feat(App): implement modal for adding new lottery with form inputs

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,89 @@
+import { Box, Button, Modal, Stack, TextField, Typography } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import React from 'react';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [lotteryName, setLotteryName] = React.useState("");
+  const [lotteryPrize, setLotteryPrize] = React.useState("");
+
+  const handleNewLottery = () => {
+    console.log(`These are the values: ${lotteryName}, ${lotteryPrize}`);
+  }
 
   return (
     <>
       <div>
         <h1>Vite + React</h1>
-        <span>React app</span>
+        
+        <Button onClick={handleOpen} variant="contained" startIcon={<Add />}>
+          Add Lottery
+        </Button>
+        
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box
+            sx={style}
+            component="form"
+            autoComplete="off"
+            onSubmit={(e: React.SubmitEvent) => {
+              e.preventDefault();
+              handleNewLottery();
+            }}>
+
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Add new lottery 
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              use this form to add a new loterry
+            </Typography>
+            <Stack spacing={2} sx={{ mt: 2}}>
+              <TextField 
+                required
+                label="Lottery name" 
+                variant="standard" 
+                value={lotteryName}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setLotteryName(event.target.value);
+                }}/>
+              <TextField 
+                required
+                label="Lottery prize" 
+                variant="standard" 
+                value={lotteryPrize}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setLotteryPrize(event.target.value);
+                }}/>              
+                <Button 
+                  variant="contained"
+                  type='submit'>
+                  New
+                </Button>
+            </Stack>
+          </Box>
+        </Modal>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
